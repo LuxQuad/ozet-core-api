3829b046fe44f2724d643e040814975dbcd609c0
Bart Kim <jaehwankim07120@gmail.com>
2021-08-11 20:24:02 +0900

    fix: config 부분 수정

--- .misc/script/build_requirement.sh
+++ .misc/script/build_requirement.sh
@@ -3,4 +3,5 @@
 set -e
 
 pip-compile --emit-index-url --header --emit-trusted-host -qo .misc/requirements/dev.txt .misc/requirements/base.in .misc/requirements/dev.in .misc/requirements/test.in
+pip-compile --emit-index-url --header --emit-trusted-host -qo .misc/requirements/test.txt .misc/requirements/base.in .misc/requirements/dev.in .misc/requirements/test.in
 pip-compile --emit-index-url --header --emit-trusted-host -qo .misc/requirements/base.txt .misc/requirements/base.in
